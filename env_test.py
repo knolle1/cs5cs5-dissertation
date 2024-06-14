ae12771be575e8c5d1873ba7fbd0d85db71b7856
Kim Nolle <nollekim@yahoo.de>
2024-06-14 11:59:03 +0100

    Test deterministic evaluation

--- env_test.py
+++ env_test.py
@@ -15,9 +15,12 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
+from agent.random import random_rollout
+
 env = gym.make('custom-parking-v0', render_mode="rgb_array")
-env.configure({"parking_angles" : [90, 90],
-               "fixed_goal" : 2})
+env.configure({"parking_angles" : [-25, 25],
+               "fixed_goal" : 2,
+               "vehicles_count" : 50})
 obs, info = env.reset()
 obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
 
@@ -26,3 +29,5 @@ for i in range(1):
     fig, ax = plt.subplots()
     ax.imshow(env.render())
     ax.axis('off')
+    
+#random_rollout(1_000, env, "./results/random", 0)
