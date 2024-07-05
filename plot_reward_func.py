f17c7700f62720100c56082d9f4b5113aa093256
Kim Nolle <nollekim@yahoo.de>
2024-07-05 01:19:53 +0100

    Update plot reward function

--- plot_reward_func.py
+++ plot_reward_func.py
@@ -8,9 +8,12 @@ Created on Tue Jun 18 18:13:11 2024
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.size'] = '16'
+
+fig, ax = plt.subplots(figsize=(10, 7))
 for p in [1, 0.5, 0.1]:
     for k in [1]:
-        x = np.arange(5, step=0.01).reshape((-1, 1))
+        x = np.arange(2, step=0.01).reshape((-1, 1))
         
         y=-k *np.power(
             np.dot(
@@ -20,7 +23,8 @@ for p in [1, 0.5, 0.1]:
             p,
         )
         
-        plt.plot(x, y, label=f"p={p} k={k}")
+        ax.plot(x, y, label=f"p={p}")
     
-plt.legend()
-plt.xlabel(r"$x = \sum \ W_i * |s_i - s_{g,i} |$")
+ax.legend()
+ax.set_xlabel("Distance between state and desired goal")
+ax.set_ylabel("Value of reward function")
